844b9a333787795043879cee722238a57ed6e431
Johan Fjeldtvedt <jaffe1@gmail.com>
2015-08-08 13:44:31 +0200

    Bring github ajour
    Last huge commit. From now on, git will be used for its purpose... This commit does the following:
      - Add old local work
      - Add GPLv3 header to all files
      - Style change: Remove typedefs of structs and enums
      - Add void to argument list of functions taking no arguments.

--- src/2a03_asm.s
+++ src/2a03_asm.s
@@ -1,10 +1,26 @@
-;;; NESIZER
+;;; Copyright (c) 2014-2015 Johan Fjeldtvedt
 ;;;
-;;; (c) 2014-2015 Johan Fjeldtvedt
+;;; This file is part of NESIZER
+;;;
+;;; NESIZER is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; NESIZER is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Foobar.  If not, see <http: //www.gnu.org/licenses/>.
+
+
+;;; 2a03_asm.s - Low level 2A03 I/O interfrace
 ;;;
 ;;; Functions for communicating with the 6502 processor in the 2A03.
 ;;; Provides functions for
-;;; 
+;;;
 ;;; 	* Setting an APU register
 ;;; 	* Disabling 6502 interrupts
 ;;; 	* Resetting the 6502 program counter
