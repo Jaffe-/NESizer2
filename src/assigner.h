4a5b9f073e073c89fa573fa1e86af252f62f20df
Johan Fjeldtvedt <jaffe1@gmail.com>
2015-08-11 22:16:17 +0200

    remove get_period from assigner

--- src/assigner.h
+++ src/assigner.h
@@ -27,7 +27,6 @@
 #pragma once
 #include <stdint.h>
 
-uint16_t get_period(uint8_t chn, uint16_t c);
 uint16_t note_to_period(uint8_t channel, uint8_t note);
 void play_note(uint8_t channel, uint8_t note);
 void stop_note(uint8_t channel);
