bcea67733b3a5e7ce9c770712298d48efa77f8d6
Johan Fjeldtvedt <jaffe1@gmail.com>
2015-12-21 22:25:26 +0100

    add licence to freqs.py

--- tools/freqs.py
+++ tools/freqs.py
@@ -1,3 +1,26 @@
+#
+#  Copyright 2014-2015 Johan Fjeldtvedt 
+
+#  This file is part of NESIZER.
+
+#  NESIZER is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  NESIZER is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with NESIZER.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+#  Period table generator
+#
+#  Generates period tables to be included as C code. 
 
 import sys
 
